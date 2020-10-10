dbf88a90a147207e726e687a2238d0a00056572e
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:19:36 +0300

    add 07.pas

---
+++ 07.pas
@@ -0,0 +1,13 @@
+﻿var p, x, S, rez: real;
+begin
+  writeln('Vvedite godovoi procent');
+  read(p);
+  writeln('Vvedite sumu, cotoraya bila vlojena');
+  read(S);
+  writeln('Vvrdite cislo let tipa dannih integer');
+ read(x);
+ p:=((S*p)/100)+S;
+ x:=p+
+ writeln(p);
+  
+end.
