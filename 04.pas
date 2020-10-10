4f54c464fe883ff8d59f69527e29ad9eaf777d09
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:16:03 +0300

    add 04.pas

---
+++ 04.pas
@@ -0,0 +1,14 @@
+﻿var N_zile: word;
+ S, Sumdn, rez: real;
+begin
+  writeln('Количество дней, которые следует посетить - ');
+  read(Sumdn);
+  writeln('Месячая плата -');
+  read(S);
+  S:=S/Sumdn;
+  writeln('Стоимость одного посещенного дня (в леях) = ',S);
+  writeln('Введите количество посещенных дней = ');
+  read(N_zile);
+  rez:=N_zile * S;
+  writeln('Месячная плата составляет = ', rez);
+end.
