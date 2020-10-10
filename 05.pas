ab9779142581dbe7f9cfe4d8dadd7cf1f5ce544b
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:16:42 +0300

    add 05.pas

---
+++ 05.pas
@@ -0,0 +1,14 @@
+﻿var a, b, sum, r, p, d: integer;
+
+begin
+  writeln('Введите 2 целых числа');
+  read(a,b);
+  sum:= a+b;
+  writeln('Сумма двух целых чисел = ', sum);
+  r:=a-b;
+  writeln('Разность двух целых чисел = ', r);
+  p:=a*b;
+  writeln('Произведение двух целых чисел = ',p);
+  d:= a div b;
+  writeln('Целая часть от деления двух целых чисел = ', d);
+end.
