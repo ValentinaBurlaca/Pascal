3b076936aa7c926185b6f2b048e8ad7663bedea5
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:21:12 +0300

    add 09.pas

---
+++ 09.pas
@@ -0,0 +1,10 @@
+﻿var C, F, K: real;
+begin
+  write('Vvedite temperaturu o Celsiu');
+  readln(C);
+  F:= (C*1.8) + 32;
+  K:= -273.15 + C;
+  writeln('Temp po Farenheitu = ', F);
+  writeln('Temp po Kelvinu = ', K);
+  
+end.
