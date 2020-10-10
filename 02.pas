7803da1730f3fb6a1e026284c362c86d46f9a140
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:13:18 +0300

    add 02.pas

---
+++ 02.pas
@@ -0,0 +1,13 @@
+﻿var a,b,x:integer;
+begin
+  writeln('introdu un numar');
+  read(a);
+  writeln('introdu al doilea numar');
+  read(b);
+  x:=b;
+  b:=a;
+  a:=x;
+  writeln('a = ',a);
+  writeln('b = ',b);
+ 
+end.
