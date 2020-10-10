7867d4e9bba58f46df2b62ed4efc8816bcefd751
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:22:01 +0300

    add 08.pas:

---
+++ 08.pas
@@ -0,0 +1,15 @@
+﻿var A1,a, b, A2: real;
+begin
+  writeln('Vvedite dlinu storoni cvadrata 1 = ');
+  read(a);
+  writeln('Vvedite dlinu storoni cvadrata 2 = ');
+  read(b);
+  A1:= sqr(a);
+  writeln('A1 = ',A1);
+  A2:=sqr(b);
+  writeln('A2 = ',A2);
+  if (A1 > A2) then writeln('A1 > A2 na ', A1 - A2)
+  else writeln('A1 < A2 na ', A2 - A1);
+  if (A1 = A2) then writeln('A1 = A2 = ',A1);
+  
+end.
