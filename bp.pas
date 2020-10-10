5b202e09c8cc5af28de7c8b439f2506a17d6cc10
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:08:56 +0300

    add bp.pas

---
+++ bp.pas
@@ -0,0 +1,10 @@
+﻿var
+  gt, vz, gr : Word;
+  begin
+    writeln('Введите текущий год ');
+    readln (gt);
+    writeln('Введите возраст приятеля ');
+    readln(vz);
+    gr:= gt-vz ;
+    writeln('Год рождения приятеля - ', gr);
+  end.
