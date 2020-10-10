f01d0e01eea68d33b392485f0ea462c3f4f8928f
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:10:02 +0300

    add n11.pas

---
+++ n11.pas
@@ -0,0 +1,8 @@
+﻿var L, Ma, Mi, J, V, S, D: integer; 
+Temp: real;
+begin
+  writeln('Introduceti temperatura a fiecarui zi, fiecare din rand nou');
+  read(L, Ma, Mi, J, V, S, D);
+  Temp:= (L + Ma + Mi + J + V + S + D)/7;
+  write('Temperatura medie a saptamanii = ', Temp);
+  end.
