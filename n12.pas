f894fac63ed15d3b78a8cde33db17cb32987869d
ValentinaBurlaca <71759599+ValentinaBurlaca@users.noreply.github.com>
2020-10-10 17:10:44 +0300

    add n12.pas

---
+++ n12.pas
@@ -0,0 +1,9 @@
+﻿var a, b, c, s: real;
+begin
+  writeln('Introdu 3 numere reale ');
+  readln(a);
+  readln(b);
+  readln(c);
+  s:=a + b + c;
+  writeln('Suma produselor este egala cu ', s);
+end.
